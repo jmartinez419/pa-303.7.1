cd9a76edf6868e7b4618afab8ab33ad98f965c10
jmartinez419 <justynmartinez419@gmail.com>
2024-07-09 10:45:34 -0400

    first commit

---
+++ src/problem1.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class problem1 {
+    public static void main(String[] args) {
+   int [] narray = {1,2,3};
+   System.out.println(Arrays.toString(narray));
+
+    }
+}
