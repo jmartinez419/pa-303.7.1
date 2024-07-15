191b1c0542f2532c5662f8e3b91d8f9d5f97a56d
jmartinez419 <justynmartinez419@gmail.com>
2024-07-15 15:36:54 -0400

    first commit

---
+++ src/problem7.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class problem7 {
+    public static void main(String [] args){
+
+        int [] array = new int[5];
+
+
+        for(int i = 0; i < array.length; i++){
+            if(i == 2){
+                continue;
+            }
+            array[i] = i + 1;
+            System.out.println(array[i]);
+            }
+        }
+
+    }
+
